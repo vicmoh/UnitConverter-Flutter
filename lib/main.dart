9bdcd98a9fd95f9ef6ee108588f2ec4cb2987189
Vicky Mohammad <vimoh1213@gmail.com>
2018-07-14 13:50:40 -0400

    gridview

--- lib/main.dart
+++ lib/main.dart
@@ -10,48 +10,7 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Udacity First App',
-      home: Scaffold(
-
-        // app bar
-        appBar: AppBar(
-          title: Center(
-            child: Text(
-              'Unit Converter',
-              textAlign: TextAlign.center,
-              textScaleFactor: 2.0,
-            ),
-          ),
-          backgroundColor: Colors.green,
-        ),
-        
-        // body
-        body: Container(
-          decoration: BoxDecoration(color: Colors.lightGreenAccent),
-          child: ListView(
-            padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-            children: <Widget>[
-              Category(
-                  icon: IconData(0xe41c, fontFamily: 'MaterialIcons'),
-                  label: "Length"),
-              Category(
-                  icon: IconData(0xe265, fontFamily: 'MaterialIcons'),
-                  label: "Area"),
-              Category(
-                  icon: IconData(0xeb44, fontFamily: 'MaterialIcons'),
-                  label: "Volume"),
-              Category(
-                  icon: IconData(0xe43d, fontFamily: 'MaterialIcons'),
-                  label: "Mass"),
-              Category(
-                  icon: IconData(0xe425, fontFamily: 'MaterialIcons'),
-                  label: "Time"),
-              Category(
-                  icon: IconData(0xe161, fontFamily: 'MaterialIcons'),
-                  label: "Digital Storage"),
-            ],
-          ),
-        ),
-      ),
+      home: CategoryRoute(),
     ),
   );
 }
--- lib/src/category.dart
+++ lib/src/category.dart
@@ -1,5 +1,61 @@
 import 'package:flutter/material.dart';
 
+class CategoryRoute extends StatelessWidget {
+
+  // dev vars 
+  final List<Category> _categories = [
+    Category(
+        icon: IconData(0xe41c, fontFamily: 'MaterialIcons'), label: "Length"),
+    Category(
+        icon: IconData(0xe265, fontFamily: 'MaterialIcons'), label: "Area"),
+    Category(
+        icon: IconData(0xeb44, fontFamily: 'MaterialIcons'), label: "Volume"),
+    Category(
+        icon: IconData(0xe43d, fontFamily: 'MaterialIcons'), label: "Mass"),
+    Category(
+        icon: IconData(0xe425, fontFamily: 'MaterialIcons'), label: "Time"),
+    Category(
+        icon: IconData(0xe161, fontFamily: 'MaterialIcons'),
+        label: "Digital Storage"),
+  ];
+
+  Widget _catagoriesListView(BuildContext context){
+    EdgeInsets topBottomPadding = new EdgeInsets.only(top: 15.0, bottom: 15.0);
+    if (MediaQuery.of(context).orientation == Orientation.portrait){
+      return ListView.builder(
+        itemBuilder: (context, index) => _categories[index],
+        itemCount: _categories.length,
+        padding: topBottomPadding,
+      );
+    }else{
+      return GridView.count(
+        crossAxisCount: 2,
+        childAspectRatio: 3.0,
+        children: _categories,
+        padding: topBottomPadding,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // app bar
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'Unit Converter',
+            textAlign: TextAlign.center,
+            textScaleFactor: 2.0,
+          ),
+        ),
+        backgroundColor: Colors.green,
+      ),
+      body: _catagoriesListView(context)
+    );
+  }
+}
+
 class Category extends StatelessWidget {
   //constructors
   const Category({
@@ -28,7 +84,7 @@ class Category extends StatelessWidget {
         // animation
         child: InkWell(
           onTap: () {
-            print("catagory button is clicked 2");
+            print("catagory button is clicked");
           },
           splashColor: Colors.green,
 
